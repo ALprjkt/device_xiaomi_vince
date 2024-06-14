5841c56e711303ea5e6994c86fc9e9d25a699c89
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-06-14 01:57:22 +0000

    vince: Update Spoof Fingerprint
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- qassa_vince.mk
+++ qassa_vince.mk
@@ -34,4 +34,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.3.0.OEGCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.019/4527419:user/release-keys"
+BUILD_FINGERPRINT := "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys"
