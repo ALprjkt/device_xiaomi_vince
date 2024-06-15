b9b8ab5ea283a17a91fed19651e6b81c15c8b4f2
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-06-15 21:20:02 +0700

    vince: add vendorsetup
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 https://github.com/ALprjkt/vendor-xiaomi-vince -b ten vendor/xiaomi/vince
+git clone --depth=1 https://github.com/ALprjkt/dc-pince -b ten device/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/ALprjkt/vc-pince -b ten vendor/xiaomi/msm8953-common
+git clone --depth=1 https://github.com/ALprjkt/kernel-xiaomi-vince kernel/xiaomi/msm8953
+git clone --depth=1 https://github.com/ALprjkt/aosp-clang prebuilts/clang/host/linux-x86/clang-aosp
