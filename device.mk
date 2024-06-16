6a390c1d73ceea1919258819d0645920b4aef146
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-06-16 13:35:58 +0700

    vince: Fix up
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -354,7 +354,7 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QMI
 PRODUCT_PACKAGES += \
